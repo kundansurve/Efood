6650547f5f0bc80855be617b9e88e9fdeba91394
Swati Mandal <Swatimandal01@gmail.com>
2022-05-21 10:05:00 +0530

    Api access

--- src/views/hotelAdmin.jsx
+++ src/views/hotelAdmin.jsx
@@ -1,193 +1,184 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import {Container,Button,Form,Tabs,Tab,Dropdown,Image} from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 // import InputBar from '../components/inputBar';
-import homeImage from '../assets/img/HomeImg.jpg';
-import './CSS files/hotelAdmin.css';
+import homeImage from "../assets/img/HomeImg.jpg";
+import "./CSS files/hotelAdmin.css";
 import Dashboard from "../views/hotelAdmin/dashboard";
 import Dishes from "../views/hotelAdmin/dishes";
 import Orders from "../views/hotelAdmin/orders";
 
-class HotelAdmin extends Component{
-    constructor(props){
-        super(props);
-        this.state={key:'Dashboard'};
-    }
+class HotelAdmin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { key: "Dashboard" };
+  }
 
-    componentDidMount(){
-        console.log("Hello")
-    }
-    render(){
-        return (
-          <div>
-            <div
-              style={{
-                backgroundImage: `url(${homeImage})`,
-                position: "absolute",
-                top: "0px",
-                left: "0px",
-                width: "100%",
-                height: "50vh",
-                zIndex: "-1",
-              }}
-            ></div>
-            <div
-              style={{
-                width: "100%",
-                height: "40vh",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                background: "rgba(0,0,0,0.6)",
-              }}
-            >
+  componentDidMount() {
+    console.log("Hello");
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            backgroundImage: `url(${homeImage})`,
+            position: "absolute",
+            top: "0px",
+            left: "0px",
+            width: "100%",
+            height: "50vh",
+            zIndex: "-1",
+          }}
+        ></div>
+        <div
+          style={{
+            width: "100%",
+            height: "40vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            background: "rgba(0,0,0,0.6)",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "white",
+              margin: "3em",
+            }}
+          >
+            <h1>Foodie</h1>
+            <span style={{ fontWeight: "1px", fontSize: "20px" }}>
+              “Ate, eating, going to eat” that’s what you must care about.
+            </span>
+            {/* <h6>Location</h6> */}
+          </div>
+        </div>
+        <div>
+          <div
+            style={{
+              display: "flex",
+              backgroundColor: "white",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <div style={{ maxWidth: "1000px", width: "100%", padding: "2em" }}>
               <div
                 style={{
                   display: "flex",
-                  flexDirection: "column",
                   justifyContent: "center",
-                  alignItems: "center",
-                  color: "white",
-                  margin: "3em",
-                }}
-              >
-                <h1>Foodie</h1>
-                <span style={{ fontWeight: "1px", fontSize: "20px" }}>
-                  “Ate, eating, going to eat” that’s what you must care about.
-                </span>
-                {/* <h6>Location</h6> */}
-              </div>
-            </div>
-            <div>
-              <div
-                style={{
-                  display: "flex",
-                  backgroundColor: "white",
-                  justifyContent: "center",
-                  alignItems: "center",
+                  alignContent: "center",
                   width: "100%",
+                  margin: "1em",
+                  padding: "1em",
                 }}
               >
-                <div
-                  style={{ maxWidth: "1000px", width: "100%", padding: "2em" }}
+                <button
+                  id="dashboard-section"
+                  style={{
+                    backgroundColor: "transparent",
+                    borderRadius: "2px",
+                    padding: "1em",
+                    paddingBottom: "0em",
+                    border:
+                      this.state.key === "Dashboard"
+                        ? "2px solid #efefef"
+                        : "none",
+                    borderBottom:
+                      this.state.key === "Dashboard"
+                        ? "none"
+                        : "2px solid #efefef",
+                  }}
+                  onClick={() => {
+                    this.setState({ key: "Dashboard" });
+                  }}
                 >
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignContent: "center",
-                      width: "100%",
-                      margin: "1em",
-                      padding: "1em",
-                    }}
-                  >
-                    <button
-                      id="dashboard-section"
-                      style={{
-                        backgroundColor: "transparent",
-                        borderRadius: "2px",
-                        padding: "1em",
-                        paddingBottom: "0em",
-                        border:
-                          this.state.key === "Dashboard"
-                            ? "2px solid #efefef"
-                            : "none",
-                        borderBottom:
-                          this.state.key === "Dashboard"
-                            ? "none"
-                            : "2px solid #efefef",
-                      }}
-                      onClick={() => {
-                        this.setState({ key: "Dashboard" });
-                      }}
-                    >
-                      Dashboard
-                    </button>
-                    <button
-                      id="dish-section"
-                      style={{
-                        backgroundColor: "transparent",
-                        borderRadius: "2px",
-                        padding: "1em",
-                        paddingBottom: "0em",
-                        border:
-                          this.state.key === "Dish"
-                            ? "2px solid #efefef"
-                            : "none",
-                        borderBottom:
-                          this.state.key === "Dish"
-                            ? "none"
-                            : "2px solid #efefef",
-                      }}
-                      onClick={() => {
-                        this.setState({ key: "Dish" });
-                      }}
-                    >
-                      Dish
-                    </button>
-                    <button
-                      id="order-section"
-                      style={{
-                        backgroundColor: "transparent",
-                        borderRadius: "2px",
-                        padding: "1em",
-                        paddingBottom: "0em",
-                        border:
-                          this.state.key === "Order"
-                            ? "2px solid #efefef"
-                            : "none",
-                        borderBottom:
-                          this.state.key === "Order"
-                            ? "none"
-                            : "2px solid #efefef",
-                      }}
-                      onClick={() => {
-                        this.setState({ key: "Order" });
-                      }}
-                    >
-                      Orders
-                    </button>
-                    <div
-                      style={{
-                        padding: "1em",
-                        paddingBottom: "0em",
-                        width: "100%",
-                        // height: "100%",
-                        borderBottom: "2px solid #efefef",
-                      }}
-                    ></div>
-                  </div>
-                  <div
-                    style={{
-                      display: this.state.key === "Dashboard" ? "" : "none",
-                      width: "95%",
-                    }}
-                  >
-                    <Dashboard />
-                  </div>
+                  Dashboard
+                </button>
+                <button
+                  id="dish-section"
+                  style={{
+                    backgroundColor: "transparent",
+                    borderRadius: "2px",
+                    padding: "1em",
+                    paddingBottom: "0em",
+                    border:
+                      this.state.key === "Dish" ? "2px solid #efefef" : "none",
+                    borderBottom:
+                      this.state.key === "Dish" ? "none" : "2px solid #efefef",
+                  }}
+                  onClick={() => {
+                    this.setState({ key: "Dish" });
+                  }}
+                >
+                  Dish
+                </button>
+                <button
+                  id="order-section"
+                  style={{
+                    backgroundColor: "transparent",
+                    borderRadius: "2px",
+                    padding: "1em",
+                    paddingBottom: "0em",
+                    border:
+                      this.state.key === "Order" ? "2px solid #efefef" : "none",
+                    borderBottom:
+                      this.state.key === "Order" ? "none" : "2px solid #efefef",
+                  }}
+                  onClick={() => {
+                    this.setState({ key: "Order" });
+                  }}
+                >
+                  Orders
+                </button>
+                <div
+                  style={{
+                    padding: "1em",
+                    paddingBottom: "0em",
+                    width: "100%",
+                    // height: "100%",
+                    borderBottom: "2px solid #efefef",
+                  }}
+                ></div>
+              </div>
+              <div
+                style={{
+                  display: this.state.key === "Dashboard" ? "" : "none",
+                  width: "95%",
+                }}
+              >
+                <Dashboard />
+              </div>
 
-                  <div
-                    style={{
-                      display: this.state.key === "Dish" ? "" : "none",
-                      width: "95%",
-                    }}
-                  >
-                    <Dishes />
-                  </div>
+              <div
+                style={{
+                  display: this.state.key === "Dish" ? "" : "none",
+                  width: "95%",
+                }}
+              >
+                <Dishes />
+              </div>
 
-                  <div
-                    style={{
-                      display: this.state.key === "Order" ? "" : "none",
-                      width: "95%",
-                    }}
-                  >
-                    <Orders />
-                  </div>
-                </div>
+              <div
+                style={{
+                  display: this.state.key === "Order" ? "" : "none",
+                  width: "95%",
+                }}
+              >
+                <Orders />
               </div>
             </div>
           </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 export default HotelAdmin;
--- src/views/hotelAdmin/orders.jsx
+++ src/views/hotelAdmin/orders.jsx
@@ -1,30 +1,65 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "@testing-library/react";
 // import "../JS files/delivery";
 import mapboxgl from "mapbox-gl";
 import Order from "../../components/Hotel/order";
 
-class Orders extends Component {
-  componentDidMount() {}
-  render() {
-    return (
-      <div
-        style={{
-          width: "100%",
-          maxWidth: "1000px",
-          margin: "auto",
-          padding: "1em",
-        }}
-      >
-        <h3 style={{padding: "0 1em 0"}}>All Orders</h3>
-        <Order status="Delivered" />
-        <Order status="Cancelled" />
-        <Order status="In Process" />
-        <Order/>
-        <Order/>
-        <Order/>
-      </div>
-    );
-  }
+function Orders() {
+  const [orderDetails, setOrderDetails] = useState({
+    orders: [],
+  });
+
+  useEffect(() => {
+    console.log("Usefect");
+    fetch("http://localhost:4000/api/hotel/dishes/6225e37a02b267ae9583f1d3", {
+      method: "GET",
+      // body: JSON.stringify(dishDetails),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Fetch Data:" + JSON.stringify(data));
+        setOrderDetails(data);
+        console.log(
+          "First element" + JSON.stringify(orderDetails["dishes"][0])
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        maxWidth: "1000px",
+        margin: "auto",
+        padding: "1em",
+      }}
+    >
+      <h3 style={{ padding: "0 1em 0" }}>All Orders</h3>
+      {/* {orderDetails.orders.map((orders, index) => {
+        console.log("ordermap " + index + JSON.stringify(orders));
+        return (
+          <Order
+            id={orders._id}
+            name={orders.name}
+            numberofRatings={orders.numberofRatings}
+            price={orders.price}
+            type={orders.type}
+          />
+        );
+      })} */}
+      <Order />
+      <Order />
+      <Order />
+      <Order />
+      <Order />
+      <Order />
+    </div>
+  );
 }
 export default Orders;
