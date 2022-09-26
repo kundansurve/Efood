16610db0180139ec656f1d66e69dfcc4eae77584
kundansurve <kundansurve01@gmail.com>
2022-09-26 12:14:36 +0530

    order tracking map fixed primarily

--- src/components/TrackMap.jsx
+++ src/components/TrackMap.jsx
@@ -30,7 +30,17 @@ class TrackMap extends Component {
         });
         //let route = `${this.state.start[0]},${this.state.start[1]};${this.state.pickUpPoint[0],this.state.pickUpPoint[1]};${this.state.dropPoint[0]},${this.state.dropPoint[1]}`;
         //if(this.state.outForDelivery)route = `${this.state.start[0]},${this.state.start[1]};${this.state.dropPoint[0]},${this.state.dropPoint[1]}`
-        fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${this.state.route}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+        
+    }
+
+componentDidUpdate(){
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: this.state.center,
+        zoom: 13
+    });
+    fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${this.state.route}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
             { method: 'GET' }
         ).then(resp => resp.json())
             .then((jsonData) => {
@@ -113,99 +123,6 @@ class TrackMap extends Component {
                 }
 
             })}).catch(error => console.log(error))
-    }
-
-componentDidUpdate(){
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v11', // style URL
-        center: this.state.center,
-        zoom: 12
-    });
-    //let route = `${this.state.start[0]},${this.state.start[1]};${this.state.pickUpPoint[0],this.state.pickUpPoint[1]};${this.state.dropPoint[0]},${this.state.dropPoint[1]}`;
-    //if(this.state.outForDelivery)route = `${this.state.start[0]},${this.state.start[1]};${this.state.dropPoint[0]},${this.state.dropPoint[1]}`
-    /*fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${this.state.route}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
-        { method: 'GET' }
-    ).then(resp => resp.json())
-        .then((jsonData) => {
-
-            const data = jsonData.routes[0];
-            
-            const Route = data.geometry.coordinates;
-            
-            const geojson = {
-                type: 'Feature',
-                properties: {},
-                geometry: {
-                    type: 'LineString',
-                    coordinates: Route
-                }
-            };
-
-            map.on('load',()=>{if (map.getSource('route')) {
-                map.getSource('route').setData(geojson);
-            }
-            // otherwise, we'll make a new request
-            else {map.addLayer({
-                    id: 'route',
-                    type: 'line',
-                    source: {
-                        type: 'geojson',
-                        data: geojson
-                    },
-                    layout: {
-                        'line-join': 'round',
-                        'line-cap': 'round'
-                    },
-                    paint: {
-                        'line-color': '#3887be',
-                        'line-width': 4,
-                        'line-opacity': 0.75
-                    }
-                });
-                map.addLayer({
-                    id: 'point',
-                    type: 'circle',
-                    source: {
-                        type: 'geojson',
-                        data: {
-                            type: 'FeatureCollection',
-                            features: [
-                                {
-                                    type: 'Feature',
-                                    properties: {},
-                                    geometry: {
-                                        type: 'Point',
-                                        coordinates: this.state.start.reverse()
-                                    }
-                                }
-                            ]
-                        }
-                    },
-                    paint: {
-                        'circle-radius': 6,
-                        'circle-color': '#3887be'
-                    }
-                });
-                const el = document.createElement('div');
-                const width = '25';
-                const height = '25';
-                el.className = 'marker';
-                el.style.backgroundImage = {bike};
-                el.style.width = `${width}px`;
-                el.style.height = `${height}px`;
-                el.style.backgroundSize = '100%';
-                 
-                
-                 
-                // Add markers to the map.
-                const marker=new mapboxgl.Marker();
-                marker.setLngLat(this.state.dropPoint.reverse())
-                .addTo(map);
-                
-            }
-
-        })}).catch(error => console.log(error))*/
 }    
     render() {
         return (<div id='map' style={{ minHeight: "60vh", width: "100%" }}></div>);
